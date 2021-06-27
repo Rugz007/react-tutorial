f16d645d05e971d9c5e213af22b4c616ba5d9448
Rugz007 <dsweb007@gmail.com>
2021-06-27 17:12:56 +0530

    Video 2

---
+++ src/components/FirstComponent.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function FirstComponent() {
+    return(
+        <div>
+            <h3>This is our first component. I am happy to see it.</h3>
+        </div>
+    )
+}
+
+export default FirstComponent
