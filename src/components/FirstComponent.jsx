6b45ac7fd5a70d7f9e25497561774cb01d4d60a7
Rugz007 <dsweb007@gmail.com>
2021-06-27 18:33:04 +0530

    Video 5

--- src/components/FirstComponent.jsx
+++ src/components/FirstComponent.jsx
@@ -1,16 +1,28 @@
-import React,{useState} from 'react'
+import React, { useState } from 'react'
 import TestComponent from './TestComponent';
 function FirstComponent(props) {
-    const [mood,setMood] = useState(props.defaultMood);
-    const changeMood = () =>
-    {
+    const [mood, setMood] = useState(props.defaultMood);
+    const changeMood = () => {
         setMood(props.newMood)
     }
-    return(
+    const displayEmote = (mood) => {
+        switch (mood) {
+            case 'happy':
+                return ( <h4>:D</h4>)
+            case 'sad':
+                return ( <h4>:((</h4>)
+            case 'angry':
+                return ( <h4>{'>:('}</h4>)
+            case 'calm':
+                return ( <h4>:|</h4>)
+        }
+    }
+    //Condition ? True : False
+    return (
         <div>
-            <TestComponent mood={mood}/>
-            <h3>Parent Component: {mood} </h3>
-            <button onClick={changeMood}>Make me {props.newMood} :)</button>
+            <h3>This is my mood: {mood} </h3>
+            {displayEmote(mood)}
+            <button onClick={changeMood}>Make me {props.newMood} {displayEmote(props.newMood)}</button>
         </div>
     )
 }
